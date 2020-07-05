6cddc2f81692bae5f09e9209874fefb4079597ce
hishambokhari <hish.b92@gmail.com>
2020-07-05 16:53:54 +0500

    Implement: Button Component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux'
+import Button from '../../UI/Button/Button';
 
 
 const orderSummary = (props) => {
@@ -13,14 +14,13 @@ const orderSummary = (props) => {
   return (
     <Aux>
       <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients</p>
+      <p>A delicious burger with the following ingredients:</p>
       <ul>
         {ingredientSummary}
       </ul>
-      <p>Continue to Checkout?
-        <button>CANCEL</button>
-        <button>CONTINUE</button>
-      </p>
+      <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   );
 };
